bef7741370e65499acd20daf9af7a15bba20a6bb
matraf123 <matraf123@gmail.com>
2022-07-12 16:51:55 -0300

    new file:   app/Http/Controllers/ClienteController.php 	modified:   app/Http/Controllers/FornecedorController.php 	new file:   app/Http/Controllers/HomeController.php 	new file:   app/Http/Controllers/LoginController.php 	new file:   app/Http/Controllers/ProdutoController.php 	modified:   app/Http/Controllers/SobreNosController.php 	modified:   app/Http/Kernel.php 	new file:   app/Http/Middleware/AutentificacaoMiddleware.php 	modified:   app/Http/Middleware/LogAcessoMiddleware.php 	new file:   resources/views/app/cliente.blade.php     new file:   resources/views/app/fornecedor.blade.php 	deleted:    resources/views/app/fornecedor/index.blade.php 	new file:   resources/views/app/home.blade.php 	new file:   resources/views/app/produto.blade.php 	new file:   resources/views/site/login.blade.php 	modified:   routes/web.php

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -16,11 +16,14 @@ class LogAcessoMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
         $ip = $request->server->get('REMOTE_ADDR');
         $rota = $request->getRequestUri();
         LogAcesso::create(['log' =>" IP $ip requisitou a rota $rota "]);
-        return $next($request);
-    
+        //return $next($request);
+
+        $resposta = $next($request);
+
+        $resposta->setStatusCode(201, 'O status da  resposta e o texto da resposta foram modificados!!');
+        return $resposta;
     }
 }
